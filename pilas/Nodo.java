e66266f215af97be8d9206c4a97fa3f28d259875
LilianaFloresMaldonado <71808888+LilianaFloresMaldonado@users.noreply.github.com>
2020-11-28 18:10:08 -0600

    Create Nodo.java

---
+++ pilas/Nodo.java
@@ -0,0 +1,13 @@
+package pilas;
+
+
+public class Nodo {
+   int  elemento;
+    Nodo siguiente;
+    
+    public Nodo(int elemento){
+        this.elemento=elemento;
+        siguiente=null;
+    }
+    
+}  
